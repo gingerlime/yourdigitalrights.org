4a0979408f90d462fd745d094795ec3ffbab0907
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2022-01-10 12:28:03 +0100

    updated wired article

--- src/components/PressCoverage/index.js
+++ src/components/PressCoverage/index.js
@@ -12,6 +12,7 @@ import GridListTile from "@material-ui/core/GridListTile";
 
 
 const items = [
+  { name: "Wired", image: "https://www.wired.com/verso/static/wired/assets/logo-header.a7598835a549cb7d5ce024ef0710935927a034f9.svg", url: "https://www.wired.com/story/delete-yourself-from-internet/" },
   { name: "Fast Company", image: "/images/press/fastcompany.png", url: "https://www.fastcompany.com/90310803/here-are-the-data-brokers-quietly-buying-and-selling-your-personal-information" },
   { name: "Business Insider", image: "/images/press/business-insider.webp", url: "https://www.businessinsider.com/how-to-delete-yourself-from-the-internet?IR=T" },
   { name: "How To Geek", image: "/images/press/howtogeek.png", url: "https://www.howtogeek.com/570124/how-to-delete-your-personal-information-from-people-finder-sites/" },
