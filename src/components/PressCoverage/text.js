1dde0fee92273b0cedea060b19755ba0616bbe18
Rafa Prada <mprada.rafael@gmail.com>
2022-01-11 13:50:53 +0100

    style changes

--- src/components/PressCoverage/text.js
+++ src/components/PressCoverage/text.js
@@ -3,6 +3,6 @@ import { FormattedMessage } from "react-intl";
 export const Title = (
   <FormattedMessage
     id="pressIntro"
-    defaultMessage="What the Press say"
+    defaultMessage="As featured on"
   />
 );
